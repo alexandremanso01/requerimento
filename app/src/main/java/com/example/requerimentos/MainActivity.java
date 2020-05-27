b0484766c9d56c97448d024a5abf530e54d0be98
alexandremanso01 <alexandre_manso01@hotmail.com>
2020-05-27 13:37:51 -0300

    todos

---
+++ app/src/main/java/com/example/requerimentos/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.requerimentos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        db.collection("users").add(user);
+
+    }
+}
